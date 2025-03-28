b622613a65845cd78a1e2c379997f66f103d6bce
dewodewo31 <dewodewo3104@gmail.com>
2025-03-28 22:18:14 +0800

    Feat: Create Mass Assignment & Relational

--- app/Models/BookingTransaction.php
+++ app/Models/BookingTransaction.php
@@ -2,9 +2,40 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BookingTransaction extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'booking_trx_id',
+        'name',
+        'phone',
+        'email',
+        'proof',
+        'total_amount',
+        'price',
+        'total_tax_amount',
+        'is_paid',
+        'started_at',
+        'wedding_package_id',
+    ];
+
+    public function weddingPackages(): BelongsTo
+    {
+        return $this->belongsTo(WeddingPackage::class, 'wedding_package_id');
+    }
+
+    public function generateUniqueTrxId()
+    {
+        $prefix = 'NWTX/INV/'; //New Wedding Transactions
+        do {
+            $randomString = $prefix . mt_rand(1000, 9999);
+        } while (self::where('booking_trx_id', $randomString)->exists());
+        return $randomString;
+    }
 }
--- app/Models/WeddingTestimonial.php
+++ app/Models/WeddingTestimonial.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class WeddingTestimonial extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'photo',
+        'wedding_package_id',
+        'message',
+        'occupation',
+    ];
+
+    public function weddingPackages(): BelongsTo
+    {
+        return $this->belongsTo(WeddingPackage::class, 'wedding_package_id');
+    }
 }
