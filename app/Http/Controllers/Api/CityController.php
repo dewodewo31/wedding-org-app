63e54b11a83f8220a8bd85b36c49546049cb5167
dewodewo31 <dewodewo3104@gmail.com>
2025-03-30 05:02:26 +0800

    Feat: Create query API for City, Wedding Organizer, and Wedding Package

--- app/Http/Controllers/Api/CityController.php
+++ app/Http/Controllers/Api/CityController.php
@@ -3,9 +3,22 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\API\CityResource;
+use App\Models\City;
 use Illuminate\Http\Request;
 
 class CityController extends Controller
 {
-    public function index() {}
+    public function index()
+    {
+        $cities = City::withCount('weddingPackages')->get();
+        return CityResource::collection($cities);
+    }
+
+    public function show(City $city)
+    {
+        $city->load(['weddingPackages.city', 'weddingPackages.photos']);
+        $city->loadCount('weddingPackages');
+        return new CityResource($city);
+    }
 }
--- app/Http/Controllers/Api/WeddingOrganizerController.php
+++ app/Http/Controllers/Api/WeddingOrganizerController.php
@@ -3,9 +3,26 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\API\WeddingOrganizerResource;
+use App\Models\WeddingOrganizer;
 use Illuminate\Http\Request;
 
 class WeddingOrganizerController extends Controller
 {
-    //
+    public function index()
+    {
+        $weddingOrganizer = WeddingOrganizer::withCount('weddingPackages')->get();
+        return WeddingOrganizerResource::collection($weddingOrganizer);
+    }
+
+    public function show(WeddingOrganizer $weddingOrganizer)
+    {
+        $weddingOrganizer->load([
+            'weddingOrganizer.photos',
+            'weddingPackages.weddingOrganizer' => function ($query) {
+                $query->withCount('weddingPackages');
+            }
+        ])->loadCount('weddingPackages');
+        return new WeddingOrganizerResource($weddingOrganizer);
+    }
 }
